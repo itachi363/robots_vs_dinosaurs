cd464dd20c953fef263702ee04f57a8e5ae4c378
itachi363 <itachi363@yahoo.com>
2022-03-03 12:07:28 -0500

    starting point

---
+++ dinosaur.py
@@ -0,0 +1,9 @@
+class Dinosaur:
+    def __init__(self):
+        self.name = ""
+        self.attack_power = 0
+        self.health = 0
+
+    def attack(self):
+        self.self = ""
+        self.robot = ""
---
+++ robot.py
@@ -0,0 +1,9 @@
+class Robot:
+    def __init__(self):
+        self.name = ""
+        self.health = 0
+        self.weapon = ""
+
+    def attack(self):
+        self.self = ""
+        self.dinosaur = ""
